1e91cde2c030f82c4b89ff0617aafc31ccf3148a
jund.fauz <mujadidjundifauzi200@gmail.com>
2024-12-08 13:47:49 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/jundfauz/dicoding/nontondulu/nonton_dulu/MainActivity.kt
@@ -0,0 +1,5 @@
+package jundfauz.dicoding.nontondulu.nonton_dulu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nonton Dulu',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Nonton Dulu'),
+      ),
+      body: const Text('test'),
+    );
+  }
+}
